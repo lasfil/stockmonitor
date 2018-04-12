b64de11ea3d4fb47e20f778a2aaafd88ccc1f85b
liuhong <iris.liu@sap.com>
2018-04-12 16:41:16 +0800

    no message

--- src/main/java/com/hungerfool/stockwatcher/controller/StockWatcherJobController.java
+++ src/main/java/com/hungerfool/stockwatcher/controller/StockWatcherJobController.java
@@ -2,21 +2,62 @@ package com.hungerfool.stockwatcher.controller;
 
 import javax.annotation.Resource;
 
+import org.quartz.JobBuilder;
+import org.quartz.JobDetail;
+import org.quartz.Scheduler;
+import org.quartz.SchedulerException;
+import org.quartz.SimpleScheduleBuilder;
+import org.quartz.Trigger;
+import org.quartz.TriggerBuilder;
+import org.quartz.impl.StdSchedulerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import com.hungerfool.stockwatcher.http.HttpAPIService;
+import com.hungerfool.stockwatcher.http.HttpService;
+import com.hungerfool.stockwatcher.job.StockWatcherJob;
 
 @RestController
-@RequestMapping(value="/job")
+@RequestMapping(value = "/job")
 public class StockWatcherJobController {
 	@Resource
-    private HttpAPIService httpAPIService;
+	private HttpService httpAPIService;
 
-    @RequestMapping("/httpclient")
-    public String test() throws Exception {
-        String str = httpAPIService.doGet("http://hq.sinajs.cn/list=sh601006,sh600000");
-        System.out.println(str);
-        return "hello";
-    }
+	// 加入Qulifier注解，通过名称注入bean
+	@Autowired
+	@Qualifier("Scheduler")
+	private Scheduler scheduler;
+
+	@RequestMapping("/httpclient")
+	public String test() throws Exception {
+		String str = httpAPIService.doGet("http://hq.sinajs.cn/list=sh601006,sh600000");
+		System.out.println(str);
+		return "hello";
+	}
+
+	@RequestMapping("/addStock")
+	public String addStock() throws SchedulerException {
+		// 启动调度器
+		scheduler.start();
+//		JobDetail jobDetail = JobBuilder.newJob().ofType(StockWatcherJob.class) // 引用Job Class
+//				.withIdentity("sh601006", "group1") // 设置name/group
+//				.usingJobData("stockCode", "sh601006") // 加入属性到ageJobDataMap
+//				.usingJobData("email", "lasfil@163.com") // 加入属性到ageJobDataMap
+//				.build();
+//
+//		// 定义一个每秒执行一次的SimpleTrigger
+//		Trigger trigger = TriggerBuilder.newTrigger().startNow().withIdentity("trigger1")
+//				.withSchedule(SimpleScheduleBuilder.simpleSchedule().withIntervalInSeconds(5).repeatForever()).build();
+//
+//		
+//
+//		try {
+//			scheduler.scheduleJob(jobDetail, trigger);
+//
+//		} catch (SchedulerException e) {
+//
+//		}
+		return "hello";
+	}
 }
--- src/main/java/com/hungerfool/stockwatcher/job/StockWatcherJob.java
+++ src/main/java/com/hungerfool/stockwatcher/job/StockWatcherJob.java
@@ -1,30 +1,50 @@
 package com.hungerfool.stockwatcher.job;
 
+import javax.annotation.Resource;
+
 import org.quartz.Job;
+import org.quartz.JobDataMap;
 import org.quartz.JobExecutionContext;
 import org.quartz.JobExecutionException;
+import org.quartz.PersistJobDataAfterExecution;
 
-public class StockWatcherJob implements Job{
+import com.hungerfool.stockwatcher.http.HttpService;
+
+@PersistJobDataAfterExecution
+public class StockWatcherJob implements Job {
 	private double maxPrice;
 	private double minPrice;
 	private String stockCode;
+	@Resource
+	private HttpService httpAPIService;
+
 	public void setStockCode(String stockCode) {
 		this.stockCode = stockCode;
 	}
+
 	public void setEmail(String email) {
 		this.email = email;
 	}
+
 	private String email;
-	
-	
+
 	public void setMaxPrice(double maxPrice) {
 		this.maxPrice = maxPrice;
 	}
+
 	public void setMinPrice(double minPrice) {
 		this.minPrice = minPrice;
 	}
+
 	@Override
 	public void execute(JobExecutionContext context) throws JobExecutionException {
-		
+		try {
+			JobDataMap dataMap = context.getMergedJobDataMap();
+
+			String str = httpAPIService.doGet("http://hq.sinajs.cn/list=" + stockCode);
+			System.out.println(str);
+		} catch (Exception e) {
+
+		}
 	}
 }
